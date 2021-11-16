e1c8094f630757a995e28b4180a9bb8c7a702e92
Carlos Galabay <cgalabay@espol.edu.ec>
2021-11-15 21:38:50 -0500

    Clase de prueba

---
+++ Proyecto1PPOO/src/main/java/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cgala
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
