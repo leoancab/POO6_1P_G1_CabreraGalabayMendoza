efa0c98df844410fd10e69e0d8ae9b1110289ade
Leonel Cabrera <leoancab@espol.edu.ec>
2021-11-20 12:44:54 -0500

    Se modifico clase Pago y se creo la clase Ruta

--- Proyecto1PPOO/src/main/java/Usuarios/Pago.java
+++ Proyecto1PPOO/src/main/java/Usuarios/Pago.java
@@ -2,13 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package Usuarios;
 
 /**
  *
  * @author leonel
  */
-public class Pago{
+public class Pago extends Cliente {
+
     private Cliente cliente;
     private Servicio servicio;
-    
+
+    public Pago(int numTarjetaCredito, int edad, Cliente cliente, Servicio servicio) {
+        super(numTarjetaCredito, edad);
+        this.cliente = cliente;
+        this.servicio = servicio;
+    }
+
 }
---
+++ Proyecto1PPOO/src/main/java/Usuarios/Ruta.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Usuarios;
+
+/**
+ *
+ * @author leonel
+ */
+public class Ruta {
+
+    private String salida;
+    private String destino;
+
+    public Ruta(String salida, String destino) {
+        this.salida = salida;
+        this.destino = destino;
+    }
+
+}
